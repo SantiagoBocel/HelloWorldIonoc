31b2b2d5adc98196d42a2f0f9cc1c1060ff60f2c
Santiago Bocel <santiagobocel10@gmail.com>
2021-07-08 14:07:44 -0600

    generate component Hello_World

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'hello-world',
+    loadChildren: () => import('./hello-world/hello-world.module').then( m => m.HelloWorldPageModule)
   },
+
 ];
 
 @NgModule({
---
+++ src/app/hello-world/hello-world.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hello-world',
+  templateUrl: './hello-world.page.html',
+  styleUrls: ['./hello-world.page.scss'],
+})
+export class HelloWorldPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
