113d544c3663c93e9a61011c1a607c603c5a37ed
Santiago Bocel <santiagobocel10@gmail.com>
2021-07-14 17:08:47 -0600

    base de datos lambda

--- src/app/cotizador/cotizador.page.ts
+++ src/app/cotizador/cotizador.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { RestProvider } from '../providers/Rest/rest';
+
 
 @Component({
   selector: 'app-cotizador',
@@ -7,19 +9,63 @@ import { RestProvider } from '../providers/Rest/rest';
   styleUrls: ['./cotizador.page.scss'],
 })
 export class CotizadorPage implements OnInit {
-
+ 
+  time: BehaviorSubject<string> = new BehaviorSubject("00")
+  timer: number;
   Criptomoneda:any;
+  Option:string;
+  Option_A:any;
+  Confirmacion:any;
   constructor(public provedor: RestProvider) {}
 
   ngOnInit() {}
-  async Cotizador(moneda){
+  async Cotizador(moneda, duracion: number){
     //debugger
-    this.provedor.Cotizar().then(data=>{
-     this.Criptomoneda = (moneda.value / data["price"]).toFixed(8);
-      console.log(this.Criptomoneda);
+     let Primero = this.Option
+     let Segunda = this.Option_A
+     let valor
+    this.provedor.Cotizar(Primero,Segunda,moneda.value).then(data=>{
+      this.Criptomoneda = data;
     }).catch(data=>{
       console.log(data);
     })
- 
+    this.timer = duracion;
+    setInterval(()=>{
+      this.updateTimevalue();
+    },1000);
+  }
+  Asignar_De(event:CustomEvent){
+    this.Option =  event.detail.value;
+  }
+  Asignar_A(event:CustomEvent){
+    this.Option_A = event.detail.value;
+  }
+
+  updateTimevalue(){
+    let seconds: any = this.timer;
+    let contador = this.timer
+    this.timer = contador - 1; 
+    if(this.timer<1){
+      this.timer = 0
+    }
+    seconds = String("0" + Math.floor(seconds)).slice(-2);
+
+    const text = seconds;
+    this.time.next(text)
+  }
+
+  async Confirmar(){
+    if(this.timer != 0){
+
+      this.Confirmacion = "Confirmado";
+      let id = String((this.timer *3));
+      let direccionIP = "192.168.56.1";
+      let browser = "Microsoft_edge";
+      let Transaccion = this.Option+this.Criptomoneda+this.Option_A;
+    this.provedor.BaseDatos(id,direccionIP,browser,Transaccion );
+    }else
+    {
+      this.Confirmacion =" ";
+    }
   }
 }
--- src/app/providers/Rest/rest.ts
+++ src/app/providers/Rest/rest.ts
@@ -15,8 +15,7 @@ import { Injectable } from "@angular/core";
             })
           };
         let postData = {Name,LastName}
-        var Api_URL = "https://gw585k8vz2.execute-api.us-east-2.amazonaws.com/Pruebas/hola-mundo";
-       
+        var Api_URL = "https://gw585k8vz2.execute-api.us-east-2.amazonaws.com/Pruebas/hola-mundo";   
           return new Promise(resolve=> {
             this.http.post(Api_URL, postData,httpOptions).subscribe(data => {
               resolve(data["body"]);
@@ -24,12 +23,34 @@ import { Injectable } from "@angular/core";
           });
    }
 
-    Cotizar(){
-    // debugger
-    var Api_URL = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT";
-    return new Promise(resolve=> {
-      this.http.get(Api_URL).subscribe(data => {resolve(data);},err=>console.log(err))
-    })
+   async BaseDatos(id,direccionIP,browser,Transaccion){
+     debugger
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json'
+        })
+      };
+    let postData = {id,direccionIP,browser,Transaccion}
+    var Api_URL = " https://gqbds2fp8a.execute-api.us-east-2.amazonaws.com/Escritura/escritura";
+        this.http.post(Api_URL, postData,httpOptions).subscribe(data=>{
+          console.log(data);
+        },err=>console.log(err))
+   }
+
+    async Cotizar(Primero,Segundo,Moneda){
+     //debugger
+     const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json'
+        })
+      };
+    let postData = {Primero,Segundo,Moneda}
+    var Api_URL = "https://pzelh4byv1.execute-api.us-east-2.amazonaws.com/Prueba_C/cotizador-criptomoneda";
+      return new Promise(resolve=> {
+        this.http.post(Api_URL, postData,httpOptions).subscribe(data => {
+          resolve(data["body"]);
+        },err=>console.log(err))
+      });
    }
 
  }
